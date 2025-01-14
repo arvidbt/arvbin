607c7bc830bf6696bef729020c200c9131163bd2
arvidbt <arvid.bergman.thorn@cygni.se>
2025-01-14 13:03:07 +0100

    rename to arvbin

--- apps/expo/eslint.config.mjs
+++ apps/expo/eslint.config.mjs
@@ -1,5 +1,5 @@
-import baseConfig from "@acme/eslint-config/base";
-import reactConfig from "@acme/eslint-config/react";
+import baseConfig from "@arvbin/eslint-config/base";
+import reactConfig from "@arvbin/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
